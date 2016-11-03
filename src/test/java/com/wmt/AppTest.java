34402024a865782d427b186e57818f1e1f2059f9
dghadge <nyy021@gmail.com>
2016-11-02 23:52:51 -0400

    Adding test suite

--- src/test/java/com/wmt/AppTest.java
+++ src/test/java/com/wmt/AppTest.java
@@ -1,38 +1,49 @@
 package com.wmt;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.wmt.controller.TicketingController;
+import com.wmt.dao.SeatHoldDAO;
+import com.wmt.model.Seats;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@InjectMocks
+	TicketingController tc;
+
+	@Mock
+	private SeatHoldDAO seatHoldDAO;
+	
+	@Before
+	public void init(){
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testApp() {
+		String result = tc.home();
+		assertEquals(result, "Welcome to your ticketing service");
+	}
+	
+	@Test
+	public void testSeatsFind(){
+		Seats seats = new Seats();
+		seats.setSeatId((long) 99);
+		
+		when(seatHoldDAO.numSeatsAvailable()).thenReturn(11);
+		
+		//verify(seatHoldDAO).numSeatsAvailable();
+		
+		assertEquals(11, tc.getNumOfSeats());
+	}
+	
+	
 }
