528248b1762085d07f79633e827f3dde6b49cd85
dghadge <nyy021@gmail.com>
2016-11-15 21:02:19 -0500

    Added second datasource which will be used exclusively for testing

--- src/main/java/com/wmt/config/AppConfiguration.java
+++ src/main/java/com/wmt/config/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.wmt.config;
 
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -23,18 +24,31 @@ public class AppConfiguration {
 		return DataSourceBuilder.create().build();
 	}
 
-	// If permissions are restricted on primary DataSource - use secondary
-	// datasource
+	// If permissions are restricted on primary DataSource - use secondary datasource
+	// In this case however we will be using a second datasource for performing our tests 
 	@Bean
-	@ConfigurationProperties(prefix = "flyway.datasource")
+	@ConfigurationProperties(prefix = "test.datasource")
 	@FlywayDataSource
 	public DataSource flywayDataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
+	public void migrateTestDB() {
+		System.out.println(">>>>>Migrating TEST DS<<<<<<>");
+		final Flyway flyway = new Flyway();
+		flyway.setDataSource(flywayDataSource());
+		flyway.migrate();
+	}
+	
 	@Bean
+	@Primary
 	public SeatHoldDAO getSeatHoldDAO() {
 		return new SeatHoldDAOImpl(dataSource());
+	}
+	
+	@Bean(name="seatHoldDAOTest")
+	public SeatHoldDAO getSeatHoldDAOTest() {
+		return new SeatHoldDAOImpl(flywayDataSource());
 	}
 
 	@Bean
--- src/test/java/com/wmt/TicketServiceDBIntegrationTest.java
+++ src/test/java/com/wmt/TicketServiceDBIntegrationTest.java
@@ -1,14 +1,15 @@
 package com.wmt;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,25 +23,26 @@ import com.wmt.model.SeatHold;
 public class TicketServiceDBIntegrationTest {
 
 	@Autowired
-	private SeatHoldDAO seatHoldDAO;
-	
+	@Qualifier("seatHoldDAOTest")
+	private SeatHoldDAO seatHoldDAOTest;
+		
 	SeatHold seathold = new SeatHold();
 
 	@Test
 	public void testNumSeatsAvailable() {
-		assertThat(seatHoldDAO.numSeatsAvailable(), is(greaterThanOrEqualTo(0)));
+		assertThat(seatHoldDAOTest.numSeatsAvailable(), is(greaterThanOrEqualTo(0)));
 	}
 	
 	@Test
 	public void testFindAndHoldSeats() {
-		SeatHold seathold = seatHoldDAO.findAndHoldSeats(1, "test@wmt.com");
+		SeatHold seathold = seatHoldDAOTest.findAndHoldSeats(1, "test@wmt.com");
 		assertThat(seathold.getSeats().size(), is(greaterThan(0)));
 	}
 	
 	@Test
 	public void testReserveSeats() {
-		SeatHold seathold = seatHoldDAO.findAndHoldSeats(1, "test@wmt.com");
-		assertThat(seatHoldDAO.reserveSeats(seathold.getSeatHoldId().intValue(), "test@wmt.com"), equalTo(seathold.getSeatHoldId().intValue()));
+		SeatHold seathold = seatHoldDAOTest.findAndHoldSeats(1, "test@wmt.com");
+		assertThat(seatHoldDAOTest.reserveSeats(seathold.getSeatHoldId().intValue(), "test@wmt.com"), equalTo(seathold.getSeatHoldId().intValue()));
 	}
 	
 }
