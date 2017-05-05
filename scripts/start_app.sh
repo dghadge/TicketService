01c2452956fc18a3f069a70d4970dc80b5fadf2a
Dnyanesh Ghadge <nyy021@gmail.com>
2017-05-05 00:12:37 -0400

    bug fix

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
+link -s /usr/javaapp/ticketingservice-1.0.1-RELEASE.war /etc/init.d/ticketserver
 
-java -jar /usr/javaapp/ticketingservice-1.0.1-RELEASE.war -Dmaven.test.skip=true > /dev/null 2> /dev/null < /dev/null &
-
-exit 0
+/etc/init.d/ticketservice restart
