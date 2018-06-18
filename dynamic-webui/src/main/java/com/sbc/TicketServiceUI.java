43427abbd1c1c0e60c8cd1e2e35e81f683b4c462
dghadge <nyy021@gmail.com>
2018-06-18 10:29:13 -0400

    renamed

---
+++ dynamic-webui/src/main/java/com/sbc/TicketServiceUI.java
@@ -0,0 +1,12 @@
+package com.sbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceUI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceUI.class, args);
+	}
+}
