b30f3201d4e51b49c9b488d3cb3f2409e56731f0
dghadge <nyy021@gmail.com>
2018-06-22 13:22:43 -0400

    fixed bug

--- cloudformation/delete-stack.sh
+++ cloudformation/delete-stack.sh
@@ -2,6 +2,6 @@
 
 export AWS_DEFAULT_REGION=us-east-2
 
-aws cloudformation delete-stack --stack-name ticketservice-vpc --capabilities CAPABILITY_IAM --profile labs
+aws cloudformation delete-stack --stack-name ticketservice-vpc --profile labs
 
 aws cloudformation delete-stack --stack-name ticketservice-eb --profile labs
