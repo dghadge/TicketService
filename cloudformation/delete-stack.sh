aac3fd06da351d7ae2bf03c60f7ccc9fd24e5e3d
dghadge <nyy021@gmail.com>
2018-06-22 13:37:49 -0400

    added wait condition

--- cloudformation/delete-stack.sh
+++ cloudformation/delete-stack.sh
@@ -2,6 +2,6 @@
 
 export AWS_DEFAULT_REGION=us-east-2
 
-aws cloudformation delete-stack --stack-name ticketservice-vpc --profile labs
-
 aws cloudformation delete-stack --stack-name ticketservice-eb --profile labs
+aws cloudformation wait stack-delete-complete --stack-name ticketservice-eb --profile labs
+aws cloudformation delete-stack --stack-name ticketservice-vpc --profile labs
